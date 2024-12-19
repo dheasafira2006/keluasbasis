085bcd918888cd338fc1e916d0dcbd2177801e5f
dheasafira2006 <ansadheasafira@gmail.com>
2024-12-19 12:10:59 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.252.1.104:3306;dbname=db_cafeshop',
+    'username' => 'pengdhea',
+    'password' => '12345',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
